804e7d5934897d525ebbc1cf7b2dfdd734ca2aa5
Tahina Joela <tahinajoela@gmail.com>
2026-05-28 09:58:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,10 @@
 const CACHE_NAME = 'mizmor-shir-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/home.html',
-    '/script.js',
-    '/style.css'
-];
+const ASSETS = ['/', '/index.html', '/script.js', '/style.css', '/manifest.json'];
 
-// Fandraisana ireo rakitra
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Fampiasana rakitra avy amin'ny cache
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then(response => response || fetch(e.request))
