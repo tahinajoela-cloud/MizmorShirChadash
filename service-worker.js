d35aaa2b39ba6ab4079c1b09576ee5b953d01a85
Tahina Joela <tahinajoela@gmail.com>
2026-05-28 13:52:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,27 @@
 const CACHE_NAME = 'mizmor-shir-v1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/home.html',
-    '/script.js',
-    '/style.css',
-    '/papaparse.min.js',
-    '/manifest.json',
-    '/icon-512.png' 
+    './',
+    'index.html',
+    'home.html',
+    'script.js',
+    'style.css',
+    'papaparse.min.js',
+    'manifest.json',
+    'icon-512.png' 
 ];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
     );
 });
