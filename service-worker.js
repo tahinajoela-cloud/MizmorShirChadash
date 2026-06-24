c4bb9cde7e1f5736c591b466be3f85840ea8a988
Tahina Joela <tahinajoela@gmail.com>
2026-06-24 14:46:48 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,48 @@
-const CACHE_NAME = 'mizmor-shir-v4';
-const ASSETS = [
+const CACHE_NAME = 'mizmor-cache-v1';
+
+// Tsy maintsy feno tsara ny rakitra rehetra ilaina eto
+const ASSETS_TO_CACHE = [
     './',
-    'index.html',
-    'home.html',
-    'script.js',
-    'style.css',
-    'papaparse.min.js',
-    'manifest.json',
-    'icon-512.png'
+    './index.html',
+    './home.html',
+    './manifest.json',
+    './style.css',
+    './script.js',
+    './papaparse.min.js',
+    './icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
     );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
     );
 });
